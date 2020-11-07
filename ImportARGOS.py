e811068c25e7d5e72fc933a92e89ad6fda6fca5a
yiwang195 <yi.wang195@duke.edu>
2020-11-07 18:42:55 -0500

    Creates empty feature class

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -13,9 +13,16 @@
 # Import modules
 import sys, os, arcpy
 
+# Allow arcpy to overwrite outputs
+arcpy.env.overwriteOutput = True
+
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+# Create an empty feature class; requires the path and name as separate parameters
+outPath,outName = os.path.split(outputFC)
+arcpy.CreateFeatureclass_management(outPath, outName)
 
 #%% Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
