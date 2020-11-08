a1713620a383f2529a7d46871edbb55d050bdd83
yiwang195 <yi.wang195@duke.edu>
2020-11-07 22:52:22 -0500

    Add fields to output feature class

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -19,10 +19,16 @@ arcpy.env.overwriteOutput = True
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+outputSR = arcpy.SpatialReference(54002)
 
 # Create an empty feature class; requires the path and name as separate parameters
 outPath,outName = os.path.split(outputFC)
-arcpy.CreateFeatureclass_management(outPath, outName)
+arcpy.CreateFeatureclass_management(outPath,outName,"POINT","","","",outputSR)
+
+# Add TagID, LC, IQ, and Date fields to the output feature class
+arcpy.AddField_management(outputFC,"TagID","LONG")
+arcpy.AddField_management(outputFC,"LC","TEXT")
+arcpy.AddField_management(outputFC,"Date","DATE")
 
 #%% Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
